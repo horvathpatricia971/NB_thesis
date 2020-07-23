4fe5b7e1d077e5219c380c1e68677e4fb001918c
horvathpatricia971 <horvathpatricia971@gmail.com>
2020-07-23 15:22:06 +0200

    Second commit.

---
+++ src/thesis/UserService.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package thesis;
+
+import java.util.ArrayList;
+import javafx.collections.ObservableList;
+
+/**
+ *
+ * @author horvathpatricia
+ */
+public class UserService {
+    
+    
+/*    public ArrayList<User> userService(){
+        DB.getAllUser();
+        return users;
+    }*/
+        
+}
